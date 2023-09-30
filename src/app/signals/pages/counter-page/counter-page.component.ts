a38e90c6aa29727f18670ddab934c9aa3f0db75a
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-30 19:25:35 +0200

    Signals: CounterPage: signal() and computed()

--- src/app/signals/pages/counter-page/counter-page.component.ts
+++ src/app/signals/pages/counter-page/counter-page.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 
 @Component({
   templateUrl: './counter-page.component.html',
-  styleUrls: ['./counter-page.component.css']
+  styleUrls: ['./counter-page.component.css'],
 })
 export class CounterPageComponent {
+  public counter = signal(0);
+  public squareCounter = computed(() => this.counter() * this.counter());
 
+  increaseBy(value: number) {
+    this.counter.update((current) => current + value);
+  }
 }
