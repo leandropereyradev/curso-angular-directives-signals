2cdce0308a631d123bcdd55c3bf4552f9274469a
Leandro Pereyra <leandro-pereyra@hotmail.es>
2023-09-30 17:22:42 +0200

    Custom directive: customLabel: @Input: change color

--- src/app/shared/directives/custom-label.directive.ts
+++ src/app/shared/directives/custom-label.directive.ts
@@ -1,21 +1,34 @@
-import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
 
 @Directive({
   selector: '[customLabel]',
 })
 export class CustomLabelDirective implements OnInit {
   private htmlElement?: ElementRef<HTMLElement>;
+  private _color: string = 'red';
+
+  @Input() set color(value: string) {
+    this._color = value;
+    this.setStyle();
+  }
 
   constructor(private el: ElementRef<HTMLElement>) {
-    console.log('Directiva: Constructor');
-    console.log({ 'Directiva constructor:': el });
+    // console.log('Directiva: Constructor');
+    // console.log({ 'Directiva constructor:': el });
 
     this.htmlElement = el;
 
-    this.htmlElement.nativeElement.innerHTML = 'Hola desde customLabel';
+    // this.htmlElement.nativeElement.innerHTML = 'Hola desde customLabel';
   }
 
   ngOnInit(): void {
-    console.log('Directiva: ngOnInit');
+    // console.log('Directiva: ngOnInit');
+    this.setStyle();
+  }
+
+  setStyle(): void {
+    if (!this.htmlElement) return;
+
+    this.htmlElement.nativeElement.style.color = this._color;
   }
 }
